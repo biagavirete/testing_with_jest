8dbbaf2cc93ad416f685ad9faf08b258c7975534
biagavirete <biagavirete@gmail.com>
2021-03-03 22:32:55 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testes com Jest</h1>
+      <p>Acesse o <a href="https://github.com/biagavirete/testing_with_jest">repositório</a> para conferir o código.</p>
     </div>
   );
 }
